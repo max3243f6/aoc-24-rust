25b15ced01f3d2e16a6df206f8ba93bc6c4612eb
max3243f6 <84030024+max3243f6@users.noreply.github.com>
2025-10-24 23:39:31 +0200

    we are making progress at reading the assignment

--- src/days/day1a.rs
+++ src/days/day1a.rs
@@ -1,18 +1,34 @@
-use std::fs;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-
+use std::io::{BufRead, BufReader};
 
 pub fn main() {
-    let reader = BufReader::new(File::open("./data/input1.txt").expect("Something went wrong reading the file"));
-    let mut num_array: Vec<Vec<usize>> = reader
+    let reader = BufReader::new(
+        File::open("./data/input1.txt").expect("Something went wrong reading the file"),
+    );
+    let input_array: Vec<Vec<usize>> = reader
         .lines()
+        // für jede zeile
         .map(|line| {
-            line.unwrap()
+            line
+                .unwrap()
                 .split_whitespace()
+                //für jede nummer in der zeile
                 .map(|s| s.parse::<usize>().unwrap())
-                .collect()
+                .collect::<Vec<usize>>()
         })
-        .collect();
-    
+        .collect::<Vec<Vec<usize>>>();
+    let mut list1: Vec<usize> = Vec::new();
+    let mut list2: Vec<usize> = Vec::new();
+    for (i, line) in input_array.iter().enumerate() {
+        list1[i] = line[0];
+        list2[i] = line[1];
+    }
+    list1.sort();
+    list2.sort();
+    let mut result = 0;
+    for i in 0..list1.len() {
+        result += (list1[i] as isize - list2[i] as isize).abs();
+    }
+
+    println!("{}", result);
 }
